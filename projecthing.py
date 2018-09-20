a9dc64234b1c9ed1fadc4bebee2321c4f0e1f1a7
Ryan Gumm <ryan.gumm@stu.jefferson.kyschools.us>
2018-09-20 09:35:48 -0400

    Commits

--- projecthing.py
+++ projecthing.py
@@ -1,27 +1,49 @@
 item_shop = {
-    'Healing Potion' : 100,
+    'Medkit' : 100,
     'Grappling Hook' : 250,
     'Level 1 Armor' : 50,
     'Level 2 Armor' : 200,
     'Level 3 Armor' : 350,
     }
 money = 500
+inventory = {
+    }
 while True:
-    answer = input("Do you want to see the (s)hop or do you wanna check (c)urrency? ")
+    answer = input("Do you want to see the (s)hop, check (c)urrency, or check (i)nventory? ")
     if answer == "s":
-        print("ITEM SHOP")
+        print("ITEM SHOP \n")
         for key, value in item_shop.items():
-            print("%s:%s" %(key, value))
+            print("%s:%s " %(key, value))
 
-        choice = input("What do you want to buy? Or do you want to see your (c)urrency? ")
-        if choice in item_shop.items():
-            print("no")
+        choice = input("What do you want to buy, your (c)urrency, or see (i)nventory? ")
+        if choice in item_shop:
+            money = money - item_shop.get(choice)
+            if money < 0:
+                    print("You can't afford that! ")
+                    money = money + item_shop.get(choice)
+            else:
+                if choice in inventory:
+                    inventory[choice] += 1
+                else:
+                    inventory[choice] = 1
+                print("You bought %s and your balance is %s" %(choice, money))
         elif choice == "c":
             print("YOUR MONEY IS %s" %(money))
+        elif choice == "i":
+            if inventory == {}:
+                print("Your inventory is empty! ")
+            else:
+                print(inventory)
         else:
-            money = money - item_shop.get(choice)
-            print("You bought %s and your balance is %s" %(choice, money))
+            print("no")
+        
+            
     if answer == "c":
         print("YOUR MONEY IS %s" %(money))
-   
-   
+    if answer == "i":
+        if inventory == {}:
+            print("Your inventory is empty! ")
+        else:
+            for key, value in inventory.items():
+                print("%s:%s" %(key, value))
+
